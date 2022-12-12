0db1cab49eec1ee75547fa79a73649dc456d9858
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2022-12-12 13:33:23 +0100

    Add tests.
    Task #067076

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part sale_credit_limit_validation module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_sale_credit_limit_validation import suite
-
-__all__ = ['suite']
