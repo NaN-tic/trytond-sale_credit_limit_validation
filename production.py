63a165a92f9f8094752dacecf7f76c6c0b7b9550
Raimon Esteve <raimonesteve@gmail.com>
2023-04-17 10:09:30 +0200

    Replace amount to 0 and check_credit_limit by party

--- production.py
+++ production.py
@@ -1,9 +1,11 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+import hashlib
 from trytond.model import dualmethod
 from trytond.pool import PoolMeta, Pool
 from trytond.exceptions import UserError
 from trytond.i18n import gettext
+from decimal import Decimal
 
 
 class Production(metaclass=PoolMeta):
@@ -20,16 +22,17 @@ class Production(metaclass=PoolMeta):
             raise UserError(gettext(
                 'sale_credit_limit_validation.msg_configuration_not_found'))
 
+        parties = set()
         for production in productions:
             if production.origin and isinstance(production.origin, SaleLine):
-                # get_credit_amount search all sales that state are ['confirmed', 'processing']
-                # Sales that come from shipments are in this state. Therefore, we must omit
-                # amounts from these sales and not sum
-                untaxed_amount = getattr(production.origin.sale,
-                    config.credit_limit_amount) * -1
-                party = production.origin.sale.party
-                # The origin is only needed to create the warning key
-                party.check_credit_limit(untaxed_amount,
-                    origin=str(production))
+                parties.add(production.origin.sale.party)
+
+        origin = hashlib.md5(
+                str(productions).encode('utf-8')).hexdigest()
+
+        for party in parties:
+            # The origin is only needed to create the warning key
+            party.check_credit_limit(Decimal(0),
+                origin=origin)
 
         return super(Production, cls).assign_try(productions)
--- shipment.py
+++ shipment.py
@@ -1,7 +1,11 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+import hashlib
 from trytond.pool import PoolMeta, Pool
 from trytond.model import dualmethod
 from trytond.exceptions import UserError
 from trytond.i18n import gettext
+from decimal import Decimal
 
 
 class ShipmentOut(metaclass=PoolMeta):
@@ -11,7 +15,6 @@ class ShipmentOut(metaclass=PoolMeta):
     def assign_try(cls, shipments):
         res = super().assign_try(shipments)
         pool = Pool()
-        SaleLine = pool.get('sale.line')
         Config = pool.get('sale.configuration')
 
         config = Config(1)
@@ -19,16 +22,11 @@ class ShipmentOut(metaclass=PoolMeta):
             raise UserError(gettext(
                 'sale_credit_limit_validation.msg_configuration_not_found'))
 
-        for shipment in shipments:
-            sales = set([
-                s.origin.sale for s in shipment.outgoing_moves if isinstance(
-                s.origin, SaleLine)])
-            # get_credit_amount search all sales that state are ['confirmed', 'processing']
-            # Sales that come from shipments are in this state. Therefore, we must omit
-            # amounts from these sales and not sum
-            untaxed_amount = sum(
-                getattr(s, config.credit_limit_amount) for s in sales) * -1
-            party = shipment.customer
-            # The origin is only needed to create the warning key
-            party.check_credit_limit(untaxed_amount, origin=str(shipment))
+        customers = list(set([s.customer for s in shipments]))
+        origin = hashlib.md5(
+                str(shipments).encode('utf-8')).hexdigest()
+
+        for customer in customers:
+            customer.check_credit_limit(Decimal(0), origin=origin)
+
         return res
